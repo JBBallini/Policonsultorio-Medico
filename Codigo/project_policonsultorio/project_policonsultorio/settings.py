be915f120a6b1195c5eb213a598107cdcb08f15f
Juan Ballini <jbballini@hotmail.com.ar>
2025-10-05 12:46:02 -0300

    Agregado CORS al back

--- Codigo/project_policonsultorio/project_policonsultorio/settings.py
+++ Codigo/project_policonsultorio/project_policonsultorio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'historiales',
     'medicos',
@@ -46,6 +47,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'project_policonsultorio.urls'
@@ -146,3 +150,7 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = "usuarios.Usuario"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
